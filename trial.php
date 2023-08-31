392e16bcaff82dd1e49ae32ba76b94f28653f8fc
Dynamite <dynamite7678@gmail.com>
2023-08-31 14:10:14 +0100

    first commit'

---
+++ trial.php
@@ -0,0 +1,26 @@
+<form method="post" action="" name="signup-form">
+<div class="form-element">
+<label>Username</label>
+<input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+</div>
+<div class="form-element">
+<label>Email</label>
+<input type="email" name="email" required />
+</div>
+<div class="form-element">
+<label>Password</label>
+<input type="password" name="password" required />
+</div>
+<button type="submit" name="register" value="register">Register</button>
+</form>
+<?php
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('HOST', 'localhost');
+    define('DATABASE', 'test');
+    try {
+        $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+?>
